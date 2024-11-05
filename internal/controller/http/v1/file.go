ff01949c23a96e8e667e96a928b941baaeb05f88
Arkhip McNutt <ortiz47@mail.ru>
2024-11-05 19:38:04 +0300

    swagger formatting

--- internal/controller/http/v1/file.go
+++ internal/controller/http/v1/file.go
@@ -24,15 +24,15 @@ func newFileRoutes(g *echo.Group, s service.Service, h *errHandler) {
 	g.DELETE("/:filename", r.delete)
 }
 
-// @Summary		Upload file to storage
-// @Description	Upload file with name
-// @Tags			files
-// @Param			file	formData	file	true	"file to be uploaded"
-// @Param			name	formData	string	true	"name of the file"
-// @Success		200		{object}	v1.fileRoutes.upload.response
-// @Failure		400		{object}	echo.HTTPError
-// @Failure		500		{object}	echo.HTTPError	"Internal error"
-// @Router			/api/v1/files [post]
+//	@Summary		Upload file to storage
+//	@Description	Upload file with name
+//	@Tags			files
+//	@Param			file	formData	file	true	"file to be uploaded"
+//	@Param			name	formData	string	true	"name of the file"
+//	@Success		200		{object}	v1.fileRoutes.upload.response
+//	@Failure		400		{object}	echo.HTTPError
+//	@Failure		500		{object}	echo.HTTPError	"Internal error"
+//	@Router			/api/v1/files [post]
 func (r *fileRoutes) upload(c echo.Context) error {
 	name := c.FormValue("name")
 	multipart, err := c.FormFile("file")
@@ -60,15 +60,15 @@ func (r *fileRoutes) upload(c echo.Context) error {
 	return c.JSON(200, "Success")
 }
 
-// @Summary		Retrieve file from storage
-// @Description	Get file by name
-// @Tags			files
-// @Param			filename	query		string	true	"name of the file"
-// @Success		200			{object}	string	"Binary file"
-// @Failure		400			{object}	echo.HTTPError
-// @Failure		404			{object}	echo.HTTPError	"Data couldn't be found"
-// @Failure		500			{object}	echo.HTTPError	"Internal error"
-// @Router			/api/v1/files/ [get]
+//	@Summary		Retrieve file from storage
+//	@Description	Get file by name
+//	@Tags			files
+//	@Param			filename	query		string	true	"name of the file"
+//	@Success		200			{object}	string	"Binary file"
+//	@Failure		400			{object}	echo.HTTPError
+//	@Failure		404			{object}	echo.HTTPError	"Data couldn't be found"
+//	@Failure		500			{object}	echo.HTTPError	"Internal error"
+//	@Router			/api/v1/files/ [get]
 func (r *fileRoutes) get(c echo.Context) error {
 	c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 	filename := c.Param("filename")
@@ -93,15 +93,15 @@ func (r *fileRoutes) get(c echo.Context) error {
 	return c.Blob(200, "application/mpeg", blob)
 }
 
-// @Summary		Delete file from storage
-// @Description	Delete file by name
-// @Tags			files
-// @Param			filename	query		string	true	"name of the file"
-// @Success		200			{object}	string
-// @Failure		400			{object}	echo.HTTPError
-// @Failure		404			{object}	echo.HTTPError	"Data couldn't be found"
-// @Failure		500			{object}	echo.HTTPError	"Internal error"
-// @Router			/api/v1/files/ [delete]
+//	@Summary		Delete file from storage
+//	@Description	Delete file by name
+//	@Tags			files
+//	@Param			filename	query		string	true	"name of the file"
+//	@Success		200			{object}	string
+//	@Failure		400			{object}	echo.HTTPError
+//	@Failure		404			{object}	echo.HTTPError	"Data couldn't be found"
+//	@Failure		500			{object}	echo.HTTPError	"Internal error"
+//	@Router			/api/v1/files/ [delete]
 func (r *fileRoutes) delete(c echo.Context) error {
 	filename := c.Param("filename")
 
